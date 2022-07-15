1cda8704e59b4f87669bdaf7351722caa67ad16c
DELL <DELL@DESKTOP-QA5IKMD>
2022-07-15 13:00:15 +0530

    Add project files.

---
+++ BlazorEditFormTutorial/Models/Character.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorEditFormTutorial.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Bio { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; } = DateTime.MinValue;
+        public string Image { get; set; } = string.Empty;
+        public int TeamId { get; set; } = 1;
+        public int DifficultyId { get; set; } = 1;
+        public bool IsReadyToFight { get; set; } =true;
+
+    }
+}
